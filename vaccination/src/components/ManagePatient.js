69f8aa84f589407cb593c54791249c168b2e6ac0
meghanach1 <122816638+meghanach1@users.noreply.github.com>
2023-12-05 21:40:57 -0600

    Update ManagePatient.js

--- vaccination/src/components/ManagePatient.js
+++ vaccination/src/components/ManagePatient.js
@@ -1,9 +1,15 @@
-
-import './css/ManagePatient.css';
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { Button, Container, Typography, Grid, Paper, createTheme, ThemeProvider } from '@mui/material';
+
+// Define a red color theme
+const redTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff1900', // Red color
+    },
+  },
+});
 
 const ManagePatient = () => {
   const location = useLocation();
@@ -12,56 +18,53 @@ const ManagePatient = () => {
 
   const handleViewProfileClick = () => {
     if (patient_id) {
-      console.log(patient_id)
       navigate(`/manage-patient/profile/${patient_id}`);
     } else {
       console.error('Patient ID is undefined.');
     }
-
   };
 
-  console.log("manage patient patientid",patient_id)
+  const handleSignOut = () => {
+    // Perform sign-out logic here, e.g., clearing session, removing tokens, etc.
+    // For simplicity, let's just redirect to the login page.
+    navigate('/');
+  };
 
   return (
-    <div>
-      <header align='center'>
-        <img
-          src={require('./images/logo.svg').default}
-          alt="Vaccination Logo"
-          className="header-logo"
-        />
-        <h1 align='center'>Manage Patient</h1>
-      </header>
+    <ThemeProvider theme={redTheme}>
+      <Container>
+        <header style={{ textAlign: 'center', margin: '20px 0' }}>
+          <img
+            src={require('./images/logo.svg').default}
+            alt="Vaccination Logo"
+            className="header-logo"
+          />
+          <Typography variant="h4">Manage Patient</Typography>
+        </header>
 
-      <div >
-        
-
-        <div >
-          <form align='center'>
-           
-          <button onClick={handleViewProfileClick}>View My Profile</button>
-      
-            <br />
-            <br />
-           
-            <button onClick={() => navigate('/manage-patient/records', { state: { patient_id } })}>
-            View My Records
-</button>
-            <br />
-            <br />
-            <button onClick={() => navigate('/schedule-appointment', { state: { patient_id } })}>
-  Schedule Appointment
-</button>
-            <br />
-            <br />
-           
-          </form>
-        
-        </div>
-       
-      </div>
-      
-    </div>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
+            <Button variant="contained" color="primary" fullWidth onClick={() => navigate('/profile', { state: { patient_id } })} style={{ marginBottom: '10px' }}>
+                View My Profile
+              </Button>
+              <br />
+              <Button variant="contained" color="primary" fullWidth onClick={() => navigate('/records', { state: { patient_id } })} style={{ marginBottom: '10px' }}>
+                View My Appointments
+              </Button>
+              <br />
+              <Button variant="contained" color="primary" fullWidth onClick={() => navigate('/schedule-appointment', { state: { patient_id } })} style={{ marginBottom: '10px' }}>
+                Schedule Appointment
+              </Button>
+              <br />
+              <Button variant="contained" color="primary" fullWidth onClick={handleSignOut}>
+                Sign Out
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 };
 
